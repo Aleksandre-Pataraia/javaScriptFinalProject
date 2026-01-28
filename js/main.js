b2e7a3967a9c6c38a705c9ee6d4373d0c3a4495b
spataraia16 <a.pataraia16@gmail.com>
2026-01-28 22:19:31 +0400

    Draft5

--- js/main.js
+++ js/main.js
@@ -11,13 +11,52 @@ fetch('https://dummyjson.com/posts', {
 .then(function(responseJS){
     console.log(responseJS)
     
+    let apiTitle = document.createElement('h1')
+    apiTitle.textContent = "ტოპ ბლოგები ზოგად თემებზე";
+    document.getElementById("fetchapi").appendChild(apiTitle);
+    
+    apiTitle.style.backgroundColor = 'cian';
+    apiTitle.style.color = '#FFA902';
+    apiTitle.style.padding = '20px';
+    apiTitle.style.fontSize = '40px';
+
     let postTitle = document.createElement("h3");
     postTitle.textContent = responseJS.posts[1].title;
     document.getElementById("fetchapi").appendChild(postTitle)
     
-    let postContent = document.createElement("h2");
+    let postContent = document.createElement("h4");
     postContent.textContent = responseJS.posts[1].body;
     document.getElementById("fetchapi").appendChild(postContent)
+
+    let postLikes = document.createElement("p");
+    postLikes.textContent = `Likes: ${responseJS.posts[1].reactions.likes}`;
+
+    let postViews = document.createElement("p");
+    postViews.textContent = `Views: ${responseJS.posts[1].views}`;
+
+    postLikes.style.display = "flex";
+    postLikes.style.position = "relative";
+    postLikes.style.right = "150px";
+    postLikes.style.backgroundColor = "#0e7a4d";
+
+    postViews.style.display = "flex";
+    postViews.style.position = "relative";
+    postViews.style.left = "140px";
+    postViews.style.bottom = "75px";
+    postViews.style.backgroundColor = "#163f91";
+
+    document.getElementById("fetchapi").appendChild(postLikes);
+    document.getElementById("fetchapi").appendChild(postViews);
+
+    let ulTitle = document.createElement('p')
+    ulTitle.textContent = "თემების არჩევანი";
+    document.getElementById("fetchapi").appendChild(ulTitle);
+    
+    ulTitle.style.backgroundColor = 'white';
+    ulTitle.style.color = 'black';
+    ulTitle.style.padding = '20px';
+    ulTitle.style.fontSize = '30px';
+
 
     let ul = document.createElement('ul');
     responseJS.posts.forEach( (post)=> {
